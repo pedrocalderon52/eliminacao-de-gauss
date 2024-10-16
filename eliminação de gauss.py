1c819758f0ed5e95f9932c94f7db93641ec0a991
Pedro Calderón <pedrocalderonn52@gmail.com>
2024-10-16 18:46:39 -0300

    adicionadas as funções de verificação de nulidade de coluna, dividir linha por constante, e mudanças na main. Agora a matriz é triangular.

--- eliminação de gauss.py
+++ eliminação de gauss.py
@@ -1,19 +1,36 @@
-def multiplicar_linhas(linha: list, constante: float):
-    return [linha[i] * constante for i in range(len(linha))]
+def zerar_item_pivo(linha_pivo: list, linha_a_zerar: list, constante: float) -> list:
+    """
+    subtrai uma linha por uma constante * linha pivô
+    linha_pivo = linha que terá os elementos zerados abaixo
+
+    isso zera o item abaixo do pivô (degrau) da escadinha
+
+
+    """
+    linha_pivo = [linha_pivo[k] * constante for k in range(len(linha_pivo))]
+    return [linha_a_zerar[k] - linha_pivo[k] for k in range(len(linha_pivo))]
+
+
+def dividir_linha(linha: list, constante: float):
+    return [linha[i] / constante for i in range(len(linha))]
+
     
 
 
 def zerar_elementos_abaixo(matriz: list, i: int, j: int):
     
-    degrau = matriz[i][j]
+    degrau = matriz[i][j] # degrau -> parte da escadinha
+    linha_degrau = matriz[i] 
 
-    if degrau != 0:
-        for k in range(i+1, len(matriz)):
-            if matriz[k][j] == 0:
-                pass
-            else:
-                constante = matriz[k][j]
-
+    
+    for k in range(i+1, len(matriz)): # percorrer os itens abaixo do degrau
+        if matriz[k][j] == 0: # verifica se o item abaixo já está zerado 
+             pass
+        else:
+            constante = matriz[k][j] / degrau
+            matriz[k] = zerar_item_pivo(linha_degrau, matriz[k], constante)
+    return matriz
+    
 
 
 def receber_matriz() -> list:
@@ -26,16 +43,16 @@ def receber_matriz() -> list:
     return matriz
 
 
-def linhas_validas(matriz:list) -> bool:
+def linhas_validas(matriz: list) -> bool:
 
     """
     verifica validade das linhas
     """
 
     tamanho = len(matriz[0])
-    if tamanho < 2: #verifica se a matriz pode ser aumentada(pelo menos 1 coluna resultado e outra dos coeficientes)
+    if tamanho < 2: # verifica se a matriz pode ser aumentada(pelo menos 1 coluna resultado e outra dos coeficientes)
         return False
-    for i in range(1, len(matriz)): #verifica se todas as linhas da matriz possuem o mesmo número de elementos
+    for i in range(1, len(matriz)): # verifica se todas as linhas da matriz possuem o mesmo número de elementos
         if len(matriz[i]) != tamanho: 
             return False
     return True
@@ -51,23 +68,53 @@ def eh_linha_nao_nula(linha: list) -> bool:
     for item in linha: saldo += item
     return bool(saldo)
 
+
+def eh_coluna_nao_nula(matriz: list, j: int) -> bool:
+
+    """
+    verifica se a coluna i da matriz recebida é não nula
+    """
+    saldo = 0
+
+    for i in range(len(matriz)): saldo += matriz[i][j]
+    return bool(saldo)
+        
+
 matriz = receber_matriz()
+        
+i=0
+for i in range(len(matriz[i])): # deleta as colunas totalmente nulas
+    if not eh_coluna_nao_nula(matriz, i): # contraintuitivo, pq i vira j
+        for j in range(len(matriz)):
+            del matriz[j][i]
 
-
-while not eh_linha_nao_nula(matriz[0]):
-    aux = matriz[0]
-    del matriz[0]
-    matriz.append(aux)
+for i in range(len(matriz)): #deleta as linhas totalmente nulas
+    if not eh_linha_nao_nula(matriz[i]):
+        del matriz[i]
     
 
-for i in range(len(matriz)):
-    j=0
-    while matriz[i][j] == 0:
-        j += 1
-    matriz[i] = dividir(matriz[i], matriz[i][j])
+i=0
+j=0
 
-if matriz[i][j] != 0:
-    k = 
+while i < len(matriz):
+    while j < len(matriz[i]) - 1:
+        if matriz[i][j] != 0:
+            matriz = zerar_elementos_abaixo(matriz, i, j)
+            matriz[i] = dividir_linha(matriz[i], matriz[i][j])
+            i += 1
+            j += 1
+            break
+        
+        else:
+            matriz.append(matriz[i])
+            del matriz[i]
+
+            
+print(matriz)
+
+    
+
+    
 
 
 
